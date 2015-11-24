574caeb0d4c2e25508170b57c779d11b835ff37b
Yinyin Qian <yyqian@live.com>
2015-11-24 14:50:57 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,20 @@ gulp.task('nodemon', function () {
 });
 
 gulp.task('lint', function () {
-    return gulp.src([
-      './controllers/*.js',
-      './middlewares/*.js',
-      './models/*.js',
-      './test/*.js',
-      './tools/*.js',
-      './*.js'
-    ])
-    // eslint() attaches the lint output to the eslint property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failOnError last.
-    .pipe(eslint.failOnError());
-  }
-);
+  return gulp.src([
+    './controllers/*.js',
+    './middlewares/*.js',
+    './models/*.js',
+    './test/*.js',
+    './tools/*.js',
+    './*.js'
+  ]).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failOnError());
+  // eslint() attaches the lint output to the eslint property
+  // of the file object so it can be used by other modules.
+  // eslint.format() outputs the lint results to the console.
+  // Alternatively use eslint.formatEach() (see Docs).
+  // To have the process exit with an error code (1) on
+  // lint error, return the stream and pipe to failOnError last.
+});
 
-gulp.task('default', ['nodemon']);
+gulp.task('default', ['lint', 'nodemon']);
